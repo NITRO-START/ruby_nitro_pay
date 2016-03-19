e666419edfd319201e33d04fb9a4a5cc2e3f5615
Ilton Garcia <ton.garcia.jr@gmail.com>
2016-03-19 15:32:06 -0300

    new generate template

--- lib/generators/templates/nitro_pay.rb
+++ lib/generators/templates/nitro_pay.rb
@@ -1,13 +1,10 @@
 # encoding: utf-8
 
-# TODO if using Rails 4 or greater use copy and paste on your secret.yml:
-# nitro_pay:
-#   app_id: # TODO your app_id
-#   app_secret_key: # TODO your app_secret_key
-# IMPORTANT: remember that test & development is not necessary if using test_env, but if you want your test app remember to use you test_app id & secret
-
-# It automatic the NitroPay to it TestEnv & ProductionEnv
-if Rails.env.development? || Rails.env.test?
+# Setup NitroPay keys
+if Rails.env.production?
+  NitroPay.app_id = Rails.application.secrets.nitro_pay.app_id
+  NitroPay.secret_key = Rails.application.secrets.nitro_pay.secret_key
+else
   # TODO if using Rails 3 or older & not using the TEST_ENV, put here your TEST app_id & your secret_key
   NitroPay.app_id = ''
   NitroPay.secret_key = ''
@@ -16,24 +13,4 @@ if Rails.env.development? || Rails.env.test?
   # NitroPay.test_env = true
   # TODO: Uncomment debugger if you have an NitroPay instance on your machine
   # NitroPay.debug = true
-elsif Rails.env.production?
-  # TODO if using Rails 3 or older, put here your PRODUCTION app_id & your secret_key
-  NitroPay.app_id = ''
-  NitroPay.secret_key = ''
-
-  # For production remember to keep it false or just remove it
-  NitroPay.debug = false
-  NitroPay.test_env = false
-end
-
-# Get your App config if your not using TEST_ENV nor DEBUGGER
-if (NitroPay.test_env.nil? && NitroPay.debug.nil?) || (NitroPay.test_env == false && NitroPay.debug == false)
-  if Rails.version[0].to_i >= 4
-    begin
-      NitroPay.app_id = Rails.application.secrets.NitroPay['app_id']
-      NitroPay.secret_key = Rails.application.secrets.NitroPay['app_secret_key']
-    rescue
-      p 'Check your Secret.yml... Please add on it your Rent$ app_id & app_secret_key'
-    end
-  end
 end
--- lib/nitro_pay/version.rb
+++ lib/nitro_pay/version.rb
@@ -1,6 +1,6 @@
 module NitroPay
   MAJOR = 1
   MINOR = 0
-  PATCH = 6
+  PATCH = 7
   VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}"
 end
