5cb8a245e781cd0a78c2a514d5306ea377f0c831
Ilton Garcia <ton.garcia.jr@gmail.com>
2016-03-23 10:30:29 -0300

    initialize template generated fixed

--- lib/generators/templates/nitro_pay.rb
+++ lib/generators/templates/nitro_pay.rb
@@ -2,8 +2,8 @@
 
 # Setup NitroPay keys
 if Rails.env.production?
-  NitroPay.app_id = Rails.application.secrets.nitro_pay.app_id
-  NitroPay.secret_key = Rails.application.secrets.nitro_pay.secret_key
+  NitroPay.app_id = Rails.application.secrets.nitro_pay['app_id']
+  NitroPay.secret_key = Rails.application.secrets.nitro_pay['secret_key']
 else
   # TODO if using Rails 3 or older & not using the TEST_ENV, put here your TEST app_id & your secret_key
   NitroPay.app_id = ''
--- lib/nitro_pay/version.rb
+++ lib/nitro_pay/version.rb
@@ -1,6 +1,6 @@
 module NitroPay
   MAJOR = 1
   MINOR = 0
-  PATCH = 7
+  PATCH = 8
   VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}"
 end
